d96718539abdb8de72c9e7361fc28726fc215ef6
Irene Lopez <elcorreodeirene@gmail.com>
2023-03-14 19:12:58 +0100

    14/03

--- src/components/Proyecto.js
+++ src/components/Proyecto.js
@@ -1,32 +1,32 @@
-import React from 'react'
+import React from "react";
 import IconoHome from "../img/icono-home.svg";
 import IconoBraces from "../img/icono-braces.svg";
 
-
-const Proyecto = ({proyecto}) => {
-
-  const {imagen, nombre, texto, web, repo}= proyecto
+const Proyecto = ({ proyecto }) => {
+  const { imagen, nombre, texto, web, repo } = proyecto;
   return (
-    <div>
-            <img
-              className="imagen-proyecto fadeTop scroll-content"
-              src={imagen}
-              alt={nombre}
-            ></img>
-            <h3>{nombre}</h3>
-            <p className="proyecto-info" >{texto}</p>
-            <div className="links-proyecto">      
-                <a className="link-pro"
-                href={web}>
-                <img src={IconoHome} alt="Ver proyecto"></img>
-                </a>
-                <a className="link-pro"
-                href={repo}>
-                <img src={IconoBraces} alt="Ver repo"></img> 
-                </a>
-            </div>
-          </div>
-  )
-}
+    <article className="articulo-proyecto">
+      <header>
+        <img
+          className="imagen-proyecto fadeTop scroll-content"
+          src={imagen}
+          alt={nombre}
+        ></img>
+      </header>
+      <main>
+        <h3 className="h3-blanco">{nombre}</h3>
+        <p className="proyecto-info">{texto}</p>
+      </main>
+      <div className="links-proyecto">
+        <a className="link-pro" href={web}>
+          <img src={IconoHome} alt="Ver proyecto"></img>
+        </a>
+        <a className="link-pro" href={repo}>
+          <img src={IconoBraces} alt="Ver repo"></img>
+        </a>
+      </div>
+    </article>
+  );
+};
 
-export default Proyecto
+export default Proyecto;
